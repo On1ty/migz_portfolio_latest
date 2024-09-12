436a04d9d493d2223bbd601fb338588775a4df8d
On1ty <juanity.andoy@gmail.com>
2024-09-12 12:04:45 +0800

    add fontawesome package latest

--- src/components/MobileNav/MobileNav.jsx
+++ src/components/MobileNav/MobileNav.jsx
@@ -1,9 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./MobileNav.css";
 
 export default function MobileNav() {
   return (
     <nav className="mobile-nav">
-      <div className="burger-menu">=</div>
+      <div className="burger-menu">
+        <FontAwesomeIcon icon={faBars} />
+      </div>
     </nav>
   );
 }
