71931093a555cb0b3473280050d54a1988de82d4
On1ty <juanity.andoy@gmail.com>
2024-09-12 14:45:41 +0800

    change layout and add MobileMenu

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css";
 import LeftSideNav from "./components/LeftSideNav/LeftSideNav";
 import Main from "./components/Main/Main";
+import MobileMenu from "./components/MobileMenu/MobileMenu";
 import MobileNav from "./components/MobileNav/MobileNav";
 import RightSideNav from "./components/RightSideNav/RightSideNav";
 
 function App() {
   return (
-    <div className="container">
+    <>
       <LeftSideNav />
       <Main />
       <RightSideNav />
       <MobileNav />
-    </div>
+      <MobileMenu />
+    </>
   );
 }
 
