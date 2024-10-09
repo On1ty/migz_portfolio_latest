bebcffef1f8b49baa5e26791f7637825967c32cd
On1ty <juanity.andoy@gmail.com>
2024-10-09 15:59:14 +0800

    left side nav info section

--- src/components/LeftSideNav/LeftSideNav.jsx
+++ src/components/LeftSideNav/LeftSideNav.jsx
@@ -2,22 +2,47 @@ import "./LeftSideNav.css";
 import avatarPhoto from "../../assets/avatar.jpeg";
 import data from "../../data.json";
 import SocialMedia from "../SocialMedia/SocialMedia";
+import moment from "moment";
 
 export default function LeftSideNav() {
-  const { firstName, lastName, currentJob, socialMedia } = data;
+  const {
+    firstName,
+    lastName,
+    currentJob,
+    socialMedia,
+    birthday,
+    city,
+    province,
+    country,
+    freelance,
+  } = data;
+  const age = moment().diff(birthday, "years");
 
   return (
     <aside className="left-sidenav">
-      <div className="profile">
-        <img className="profile__avatar" src={avatarPhoto} alt="avatar" />
-        <p className="profile__name">
-          {firstName} {lastName}
-        </p>
-        <span className="profile_job">{currentJob}</span>
-        <SocialMedia socialMedia={socialMedia} />
+      <div className="wrapper">
+        <div className="profile">
+          <img className="profile__avatar" src={avatarPhoto} alt="avatar" />
+          <p className="profile__name">
+            {firstName} {lastName}
+          </p>
+          <span className="profile_job">{currentJob}</span>
+          <SocialMedia socialMedia={socialMedia} />
+        </div>
+        <div className="info">
+          <div><span>Age</span></div>
+          <span>{age}</span>
+          <div><span>Residence</span></div>
+          <span>{country}</span>
+          <div><span>Freelance</span></div>
+          <span className={freelance ? "freelance" : "freelance--error"}>{freelance ? "" : "Not"} Available</span>
+          <div><span>Address</span></div>
+          <span>
+            {city}, {province}
+          </span>
+        </div>
+        <div></div>
       </div>
-      <div></div>
-      <div></div>
     </aside>
   );
 }
